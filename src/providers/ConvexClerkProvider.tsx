a671feb3887e322c491c4bcc48a90bb3733bcad9
Tiago Lima <tiagolima@osf.digital>
2026-05-20 15:03:39 -0300

    chore: Integrated with convex database. Defined the schema. Created providers.

---
+++ src/providers/ConvexClerkProvider.tsx
@@ -0,0 +1,23 @@
+"use client";
+import { useAuth } from "@clerk/react";
+import { ClerkProvider } from "@clerk/nextjs";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+const convexClient = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+export default function ConvexClerkProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY!}
+    >
+      <ConvexProviderWithClerk client={convexClient} useAuth={useAuth}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
